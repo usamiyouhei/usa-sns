ebf21e99c0d3f864c5e4218fcf47a4c3b41ff9c2
youhei usami <usao9110@icloud.com>
2025-08-19 08:47:38 +0400

    8/19

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express()
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
 const PORT = 3333;
+
+
+// ミドルウェア
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
 
 app.get("/", (req, res) => {
   res.send("hello express")
 })
+
+// app.get("/users", (req, res) => {
+//   res.send("users express")
+// })
 
 app.listen(PORT, () => console.log("サーバーが起動しました"))
