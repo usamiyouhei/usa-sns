f55eeaeff36b36f6a9f95b852e1a39fd26a5c658
youhei usami <usao9110@icloud.com>
2025-08-24 11:08:20 +0400

    8/24

--- routes/users.js
+++ routes/users.js
@@ -38,11 +38,42 @@ router.delete("/:id", async(req, res) => {
 // User info get
 router.get("/:id", async(req, res) => {
     try {
-      const user = await User.findById(req.params.id)
-      res.status(200).json(user)
+      const user = await User.findById(req.params.id);
+      const { password, updatedAt, ...other} = user._doc;
+      res.status(200).json(other)
     } catch (err) {
       return res.status(500).json(err)
     }
+})
+
+// User follow
+router.put("/:id/follow", async (req, res) => {
+  if(req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id)
+      const currentUser = await User.findById(req.body.userId);
+      // フォロワーに自分がいなかったらフォローできる
+      if(!user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $push: {
+            followings: req.params.id,
+          }
+        })
+        return res.status(200).json("フォローに成功しました！")
+      } else {
+        return res.status(403).json("あなたはすでにこのユーザーをフォローしています")
+      }
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res.status.json("自分自身をフォローできません。")
+  }
 })
 
 // router.get("/", (req, res) => {
