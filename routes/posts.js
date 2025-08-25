bbd0fc558567e4d5eb26ba8b3f6a3771de451e07
youhei usami <usao9110@icloud.com>
2025-08-26 00:31:30 +0400

    8/26

--- routes/posts.js
+++ routes/posts.js
@@ -29,6 +29,23 @@ router.put("/:id", async(req,res) => {
   }
 })
 
+// post delete
+router.put("/:id", async(req,res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("投稿編集に成功しました！")
+    } else {
+      return res.status(403).json("あなたは他の人の投稿を編集できません")
+    }
+  } catch (err) {
+    return res.status(403).json(err)
+  }
+})
+
 // router.get("/", (req, res) => {
 //   res.send("posts router")
 // })
