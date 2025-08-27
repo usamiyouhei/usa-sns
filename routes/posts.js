0a16e13fc6727a4d9da8010230c3bd7c0b737198
youhei usami <usao9110@icloud.com>
2025-08-27 11:20:58 +0400

    8/27

--- routes/posts.js
+++ routes/posts.js
@@ -54,6 +54,33 @@ router.get("/:id", async(req,res) => {
 })
 
 // 特定の投稿にいいねを押す
+router.put("/:id/like", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id)
+      // まだ投稿にいいねが押されていなかったら
+      if(!post.likes.includes(req.body.userId)) {
+        await post.updateOne({
+          $push: {
+            likes: req.body.userId,
+          },
+        });
+        return res.status(200).json("投稿にいいねを押しました！")
+        // 投稿にすでにいいねが押されていたら
+      } else {
+        // いいねしているユーザーIDを取り除く
+        await post.updateOne({
+          $pull: {
+            likes: req.body.userId,
+          }
+        })
+        return res
+        .status(403)
+        .json("投稿にいいねを外しました")
+      }
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+})
 
 // router.get("/", (req, res) => {
 //   res.send("posts router")
